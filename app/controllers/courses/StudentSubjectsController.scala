dc5fc932f23a9ba85ef3554617a400c9742ca521
MillerThomas <33414062+MillerThomas@users.noreply.github.com>
2018-09-24 12:11:08 +0200

    Update StudentSubjectsController.scala

--- app/controllers/courses/StudentSubjectsController.scala
+++ app/controllers/courses/StudentSubjectsController.scala
@@ -4,3 +4,23 @@
   *Reviewer Dorcas
   *
   */
+
+
+
+
+package controllers.people
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+/**
+  * Created Thomas on 2018/09/24
+  */
+class StudentSubjectsController @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
+ 
+ def index = Action {
+    Ok("Stusent Subject controller is working")
+}
+
+
+}
