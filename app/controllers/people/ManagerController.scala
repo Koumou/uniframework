f13fcb7697ca9bcccbb597f789f9b8b4433f025c
MillerThomas <33414062+MillerThomas@users.noreply.github.com>
2018-09-24 12:00:15 +0200

    Update ManagerController.scala

--- app/controllers/people/ManagerController.scala
+++ app/controllers/people/ManagerController.scala
@@ -4,3 +4,20 @@
   *Reviewer Wailed
   *
   */
+
+packagecontrollers.manager
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+/**
+  * Created Thomas on 2018/09/24
+  */
+class ManagerController @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
+ 
+ def index = Action {
+    Ok("Manager controller is working")
+}
+
+
+}
